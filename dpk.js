39f2b047e84aff21fc283990d20e15ff91b6288e
Felipe Braga <contato@fbsis.com>
2023-02-18 19:15:16 -0300

    refactor: create a easily function to be hash values

--- dpk.js
+++ dpk.js
@@ -1,45 +1,51 @@
 const crypto = require("crypto");
 
+const defaultHashAlgorithm = "sha3-512";
+
 const TRIVIAL_PARTITION_KEY = "0";
 const MAX_PARTITION_KEY_LENGTH = 256;
+
+const createHash = (data, hash = defaultHashAlgorithm) => crypto.createHash(hash).update(data).digest("hex");
 
 const handlerEventCandidate = (event) => {
   if (event) {
     if (event.partitionKey) return event.partitionKey;
 
     const data = JSON.stringify(event);
-    return crypto.createHash("sha3-512").update(data).digest("hex");
+    return createHash(data);
   }
 };
 
 const truncatePartitionKey = (candidate, maxLength) => {
-  if (candidate && candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    return crypto.createHash("sha3-512").update(candidate).digest("hex");
+  if (candidate && candidate.length > maxLength) {
+    return createHash(candidate);
   }
   return candidate;
 }
 
-const  stringifyPartitionKey = (candidate) => {
+const  stringifyPartitionKey = (candidate, trivialPartitionKey) => {
   if (candidate && typeof candidate !== "string") {
     return JSON.stringify(candidate);
   }
-  return candidate || TRIVIAL_PARTITION_KEY;
+  return candidate || trivialPartitionKey;
 }
 
-const handlerMaxCandidateLengh = (candidate) => {
-  if(candidate.length > MAX_PARTITION_KEY_LENGTH){
-    return crypto.createHash("sha3-512").update(candidate).digest("hex");
+const handlerMaxCandidateLengh = (candidate, maxLength) => {
+  if(candidate.length > maxLength){
+    return createHash(candidate);
   }
   return candidate
 };
 
 exports.deterministicPartitionKey = (event) => {
-  let candidate;
+  let candidate = null;
+
+  if (!event) return "0";
 
   candidate = handlerEventCandidate(event);
-  candidate = truncatePartitionKey(candidate);
-  candidate = stringifyPartitionKey(candidate);
-  candidate = handlerMaxCandidateLengh(candidate);
+  candidate = truncatePartitionKey(candidate, MAX_PARTITION_KEY_LENGTH);
+  candidate = stringifyPartitionKey(candidate, TRIVIAL_PARTITION_KEY);
+  candidate = handlerMaxCandidateLengh(candidate, MAX_PARTITION_KEY_LENGTH);
 
   return candidate;
 };
