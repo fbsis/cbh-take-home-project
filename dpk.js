502bd70ec5af0be51ab5c0ef20a156631ec9ccf1
Felipe Braga <contato@fbsis.com>
2023-02-18 18:50:37 -0300

    reafactor: dpk file to a clear

--- dpk.js
+++ dpk.js
@@ -1,28 +1,45 @@
 const crypto = require("crypto");
 
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const handlerEventCandidate = (event) => {
+  if (event) {
+    if (event.partitionKey) return event.partitionKey;
+
+    const data = JSON.stringify(event);
+    return crypto.createHash("sha3-512").update(data).digest("hex");
+  }
+};
+
+const truncatePartitionKey = (candidate, maxLength) => {
+  if (candidate && candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    return crypto.createHash("sha3-512").update(candidate).digest("hex");
+  }
+  return candidate;
+}
+
+const  stringifyPartitionKey = (candidate) => {
+  if (candidate && typeof candidate !== "string") {
+    return JSON.stringify(candidate);
+  }
+  return candidate || TRIVIAL_PARTITION_KEY;
+}
+
+const handlerMaxCandidateLengh = (candidate) => {
+  if(candidate.length > MAX_PARTITION_KEY_LENGTH){
+    return crypto.createHash("sha3-512").update(candidate).digest("hex");
+  }
+  return candidate
+};
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
+  candidate = handlerEventCandidate(event);
+  candidate = truncatePartitionKey(candidate);
+  candidate = stringifyPartitionKey(candidate);
+  candidate = handlerMaxCandidateLengh(candidate);
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
   return candidate;
 };
